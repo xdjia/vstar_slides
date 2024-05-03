15274c037c47843bca092771924e7ee7186e1565
Xiaodong Jia <jiaxiaodong1994@gmail.com>
2024-05-03 14:19:48 -0400

    update performance table

--- calc.js
+++ calc.js
@@ -270,7 +270,7 @@ function init_vtable2() {
 
     update_my_table_body(table_id, 0, 0, "$\\epsilon$")
     for (let i of [1, 2, 3]) {
-        vrow = i === 1 ? '$\\epsilon$' : '$' + rows[i] + '$'
+        vrow =  '$' + rows[i] + '$'
         update_my_table_body(table_id, i, 0, vrow)
 
         // table1.rows[k].style.visibility = "hidden";
